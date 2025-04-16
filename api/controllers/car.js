428c838e64c758e4d61af759da767c816a464759
Posenega <theokhalil0@icloud.com>
2025-04-16 19:18:24 +0300

    Refactor response structure in getAll function to include success message

--- api/controllers/car.js
+++ api/controllers/car.js
@@ -73,10 +73,10 @@ async function getAll(req, res) {
       return res
         .status(404)
         .json({ message: "No cars found", cars: [] })
-    return res.status(200).json({ cars })
+    return res.status(200).json({ message: "success", cars })
   } catch (err) {
     console.log(err)
-    return res.status(500).json({ message: err.message })
+    return res.status(500).json({ message: err.message, cars: [] })
   }
 }
 
